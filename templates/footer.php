f9630c28c4c2072a3df141673c7b4616d73c88aa
Tom Hummel <tom@interlincx.com>
2013-08-25 13:31:49 -0700

    moved header and footer to templates/. added h5bp html to header

---
+++ templates/footer.php
@@ -0,0 +1,45 @@
+
+
+</td>
+
+<tr><td valign="top">
+	<table>
+		
+			<td>
+			Match:
+			<table>
+			<tr><td><a href="match.php">Add New</a></td></tr>
+			</table>
+			</td>
+
+<tr><td></td></tr>
+
+<td>
+Reports:
+<table>
+
+<tr><td><a href="rptSummary.php">Summary</a></td></tr>
+<tr><td><a href="rptPerformance.php">Performance</a></td></tr>
+<tr><td><a href="rptCollab.php">Match Strength</a></td></tr>
+<tr><td><a href="rptPerfDist.php">Win Expectancy</a></td></tr>
+
+<tr><td></td></tr>
+
+				</table>
+			</td>
+		</tr>
+	</table>
+</td>
+</tr>
+<tr><td align="center" colspan="2">Copyright 2007 Tom Hummel</td></tr></table>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43473175-1', 'tphum.us');
+  ga('send', 'pageview');
+
+</script>
---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php echo $title ?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/tnt.css" />
+    <script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
+  </head>
+
+  <body>
+    <table width="100%" border="1" cellpadding="10">
+      <tr>
+        <td align="center" valign="top" width="15%" height="50px">
+          <a href="index.php"><img src="assets/img/tntbox.JPG"></a>
+        </td>
+        <td valign="top" width="85%" rowspan="2">
