6fc71f490b9782adfcbf21939198908ea4adeef2
Tom Hummel <tom@interlincx.com>
2013-08-31 15:58:20 -0700

    test: basic testing framework ready

---
+++ test/runner.php
@@ -0,0 +1,46 @@
+<?php
+$dir = dirname ( __FILE__  ) ;
+
+require "assert.php" ;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$specFiles = array (
+  "control"
+) ;
+
+$passCount = 0 ;
+$errors = array ( ) ;
+$fileCount = count ( $specFiles ) ;
+$testCount = 0 ; 
+
+foreach ($specFiles as $file) {
+  $path = $dir . "/specs/" . $file . ".php" ; 
+  
+  include_once $path ;
+
+  foreach ($tests as $test) {
+    $testCount ++ ;
+    try {
+      $test ( ) ;
+      $passCount++ ;
+    }catch(Assert\InvalidArgumentException $e) {
+      array_push ( $errors, $e ) ;
+    }
+
+  }
+
+}
+
+echo "<div>files: $fileCount</div>" ;
+echo "<div>tests: $testCount</div>" ;
+echo "<div>pass: $passCount</div>" ;
+$failCount = count ( $errors ) ;
+echo "<div>fail: $failCount </div>" ;
+foreach ($errors as $error) {
+  echo "<div>$error</div>" ;
+}
+
+
+?>
