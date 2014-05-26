cac0f1020de2e0e9d3c18064eb98f7a6796206ea
Tom Hummel <tphummel@gmail.com>
2014-05-26 13:36:10 -0700

    test/assert: added default $desc vals

--- test/assert.php
+++ test/assert.php
@@ -1,19 +1,19 @@
 <?php
 
 Class Assert {
-  public static function ok ($test, $desc) {
+  public static function ok ($test, $desc="") {
     if($test == false) {
       $msg = 'Assert::ok failed: '.$desc. ' , value: '.$test;
       throw new Exception($msg);
     }
   }
-  public static function notOk ($test, $desc) {
+  public static function notOk ($test, $desc="") {
     if($test == true) {
       $msg = 'Assert::notOk failed: '.$desc. ' , value: '.$test;
       throw new Exception($msg);
     }
   }
-  public static function equal ($value, $expected, $desc) {
+  public static function equal ($value, $expected, $desc="") {
     if($value != $expected) {
       $msg = 'Assert::equal failed: '.$desc. ' , value: '.$value.' , expected: '.$expected;
       throw new Exception($msg);
