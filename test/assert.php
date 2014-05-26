c531cd3e596fe3b7649bb194b867819259528b7f
Tom Hummel <tphummel@gmail.com>
2014-05-26 13:11:46 -0700

    test/assert: rolled my own Assert class w/ ok, notOK

--- test/assert.php
+++ test/assert.php
@@ -1,11 +1,17 @@
 <?php
-$dir = dirname ( __FILE__  ) ;
 
-$assertRoot = $dir . "/vendor/assert/lib/Assert/" ;
-require $assertRoot . "Assertion.php" ;
-require $assertRoot . "AssertionFailedException.php" ;
-require $assertRoot . "InvalidArgumentException.php" ;
-
-use Assert\Assertion ;
-
+Class Assert {
+  public static function ok ($test, $desc) {
+    if($test == false) {
+      $msg = 'Assert::ok failed: '.$desc. ' , value: '.$test;
+      throw new Exception($msg);
+    }
+  }
+  public static function notOk ($test, $desc) {
+    if($test == true) {
+      $msg = 'Assert::notOk failed: '.$desc. ' , value: '.$test;
+      throw new Exception($msg);
+    }
+  }
+}
 ?>
